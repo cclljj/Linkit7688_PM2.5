3db948aa1e9f998a8cc8e4396c97774c0812d992
Ling-Jyh Chen <cclljj@gmail.com>
2017-08-05 03:30:42 +0000

    add support to g5 and a4 sensor

--- air.py
+++ air.py
@@ -1,10 +1,11 @@
 import mraa
-#import mqtt
+import mqtt
 import time
+from datetime import datetime
 
-#import g3 as air_sensor
-import htu21d as tmp_sensor
-import bh1750fvi as light_sensor
+import pm_g3 as air_sensor
+import th_htu21d as tmp_sensor
+import light_bh1750fvi as light_sensor
 from multiprocessing import Queue
 
 
@@ -18,31 +19,37 @@ if __name__ == '__main__':
 	tmp_q = Queue()
 	light_q = Queue()
 
-	#air = air_sensor.sensor(air_q)
+	air = air_sensor.sensor(air_q)
 	tmp = tmp_sensor.sensor(tmp_q)
 	light = light_sensor.sensor(light_q)
 
-	#air.start()
+	air.start()
 	tmp.start()
 	light.start()
 
+	pat = 'PM1.0={pm1_0},PM2.5={pm2_5},PM10={pm10},Tmp={tmp:5.3f},RH={rh},Lux={lux}'
+	#pat = 'Tmp={tmp:5.3f},RH={rh},Lux={lux}'
 	#pat = 'PM1.0={pm1_0},PM2.5={pm2_5},PM10={pm10},Tmp={tmp:5.3f},RH={rh}'
-	pat = 'Tmp={tmp:5.3f},RH={rh},Lux={lux}'
+
 	tmp_data = {'Tmp':0.0, 'RH':0}
-	
 
 	sn = 1
 	while True:
-		#air_data = air_q.get()
-		light_data = light_q.get()
-		
+		flag = 0
+		if not air_q.empty():
+			air_data = air_q.get()
+			flag = flag + 1
+		if not light_q.empty():
+			light_data = light_q.get()
+			flag = flag + 1
 		if not tmp_q.empty():
 			tmp_data = tmp_q.get()
-
+			flag = flag + 1
+		if flag==3:
 			send_str = pat.format(
-		#	    pm1_0=air_data['CFPM1.0'],
-		#	    pm2_5=air_data['CFPM2.5'],
-		#   	    pm10=air_data['CFPM10'],
+			    pm1_0=air_data['PM1.0'],
+			    pm2_5=air_data['PM2.5'],
+		   	    pm10=air_data['PM10'],
 			    tmp=tmp_data['Tmp'],
 			    rh=tmp_data['RH'],
 			    lux = light_data['Lux'],
@@ -51,5 +58,7 @@ if __name__ == '__main__':
 			#mqtt.mqtt().pub(send_str)
 			print send_str
 			print sn
+			print datetime.utcnow()
 			sn = sn + 1
+		time.sleep(2)
 
